665820be98eb7e9e716debd52c106ae705edddf3
Tom Brand <tom025@me.com>
2012-10-01 08:34:33 +0000

    Login to fake XMPP server.

--- spec/features/auction_sniper_joins_auction_and_looses_spec.rb
+++ spec/features/auction_sniper_joins_auction_and_looses_spec.rb
@@ -1,7 +1,41 @@
+require 'java'
+require 'org/smack/smack.jar'
+require 'org/smack/smackx.jar'
 require 'rspec'
 require 'rspec/given'
 
-describe 'AuctionSniper' do
+module Smack
+  include_package 'org/jivesoftware/smack'
+  java_import org.jivesoftware.smack.XMPPConnection
+end
+
+module AuctionSniper; end
+
+class FakeAuctionServer
+  ITEM_ID_AS_LOGIN = 'auction-%s'
+  XMPP_HOSTNAME = 'localhost'
+  AUCTION_PASSWORD = 'auction'
+  AUCTION_RESOURCE = 'Auction'
+
+  def initialize(item_id)
+    @item_id = item_id
+    @connection = Smack::XMPPConnection.new(XMPP_HOSTNAME)
+  end
+
+  def start_selling_item
+    @connection.connect
+    @connection.login(ITEM_ID_AS_LOGIN % @item_id,
+                      AUCTION_PASSWORD,
+                      AUCTION_RESOURCE)
+  end
+end
+
+class ApplicationRunner
+  def start_bidding_in(auction)
+  end
+end
+
+describe AuctionSniper do
   after do
     auction.stop
     application.stop
