bce7d9fe6516e4b9c06d2778e1205c1f8555a636
Tom Brand <tom025@me.com>
2012-10-07 15:29:22 +0000

    Move WindowLicker to own file

---
+++ lib/window_licker.rb
@@ -0,0 +1,14 @@
+require 'org/hamcrest/hamcrest-all-SNAPSHOT.jar'
+require 'org/window_licker/windowlicker-swing-DEV.jar'
+require 'org/window_licker/windowlicker-core-DEV.jar'
+
+module WindowLicker
+  include_package 'com/objogate/wl'
+  include_package 'com/objogate/wl/swing'
+  java_import com.objogate.wl.swing.driver.JFrameDriver
+  java_import com.objogate.wl.swing.driver.JLabelDriver
+  java_import com.objogate.wl.swing.gesture.GesturePerformer
+  java_import com.objogate.wl.swing.AWTEventQueueProber
+end
+
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,7 @@
 require 'java'
-require 'org/window_licker/windowlicker-swing-DEV.jar'
-require 'org/window_licker/windowlicker-core-DEV.jar'
-require 'org/hamcrest/hamcrest-all-SNAPSHOT.jar'
+require 'lib/window_licker'
 require 'rspec'
 require 'rspec/given'
-
-module WindowLicker
-  include_package 'com/objogate/wl'
-  include_package 'com/objogate/wl/swing'
-  java_import com.objogate.wl.swing.driver.JFrameDriver
-  java_import com.objogate.wl.swing.driver.JLabelDriver
-  java_import com.objogate.wl.swing.gesture.GesturePerformer
-  java_import com.objogate.wl.swing.AWTEventQueueProber
-end
 
 module Hamcrest
   java_import org.hamcrest.Matchers
