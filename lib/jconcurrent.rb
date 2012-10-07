5af8da24edf24663bbd82a2c994e2b83ce5aeb79
Tom Brand <tom025@me.com>
2012-10-07 18:19:29 +0000

    Move jconcurrent to own file.

---
+++ lib/jconcurrent.rb
@@ -0,0 +1,6 @@
+require 'java'
+
+module JConcurrent
+  java_import java.util.concurrent.ArrayBlockingQueue
+  java_import java.util.concurrent.TimeUnit
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,6 @@
 require 'java'
 require 'lib/window_licker'
 require 'lib/hamcrest'
+require 'lib/jconcurrent'
 require 'rspec'
 require 'rspec/given'
-
-module JConcurrent
-  java_import java.util.concurrent.ArrayBlockingQueue
-  java_import java.util.concurrent.TimeUnit
-end
