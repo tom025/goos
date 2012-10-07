4122f5d7d49e12cb8bd1dfa93b8b696e90561aea
Tom Brand <tom025@me.com>
2012-10-07 14:00:25 +0000

    Sniper joins then looses auction

--- spec/features/auction_sniper_joins_auction_and_looses_spec.rb
+++ spec/features/auction_sniper_joins_auction_and_looses_spec.rb
@@ -45,8 +45,6 @@ end
 
 module AuctionSniper
   MAIN_WINDOW_NAME = 'Auction Sniper'
-  STATUS_JOINING = 'joining'
-  STATUS_LOST = 'lost'
   SNIPER_STATUS_NAME = 'status'
   AUCTION_RESOURCE = 'Auction'
   ITEM_ID_AS_LOGIN = 'auction-%s'
@@ -63,13 +61,18 @@ module AuctionSniper
     end
 
     def start_user_interface
-      Swing::SwingUtilities.invoke_and_wait(Proc.new { @ui = MainWindow.new })
+      Swing::SwingUtilities.invoke_and_wait do
+        @ui = MainWindow.new
+      end
     end
 
     def join_auction(connection, item_id)
-      chat = connection.get_chat_manager.create_chat(auction_id(item_id, connection)) do |a_chat, message|
-        # nothing yet
-      end
+      chat = connection.get_chat_manager.
+        create_chat(auction_id(item_id, connection)) do |a_chat, message|
+          Swing::SwingUtilities.invoke_later do
+            @ui.show_status(MainWindow::STATUS_LOST)
+          end
+        end
       chat.send_message(Smack::Message.new)
     end
 
@@ -78,30 +81,37 @@ module AuctionSniper
       AUCTION_ID_FORMAT % [item_id, connection.get_service_name]
     end
 
-    class MainWindow < Swing::JFrame
-      attr_accessor :sniper_status
-
-      def initialize
-        super("Auction Sniper")
-        @sniper_status = create_label(STATUS_JOINING)
-        set_name(MAIN_WINDOW_NAME)
-        add(sniper_status)
-        pack
-        set_default_close_operation(Swing::JFrame::EXIT_ON_CLOSE)
-        set_visible(true)
-      end
-
-      private
-      def create_label(initial_text)
-        label = Swing::JLabel.new(initial_text)
-        label.set_name(SNIPER_STATUS_NAME)
-        label.set_border(Swing::LineBorder.new(AWT::Color::BLACK))
-        label
-      end
-    end
   end
 
   private
+  class MainWindow < Swing::JFrame
+    STATUS_JOINING = 'joining'
+    STATUS_LOST = 'lost'
+
+    attr_accessor :sniper_status
+
+    def initialize
+      super("Auction Sniper")
+      @sniper_status = create_label(STATUS_JOINING)
+      set_name(MAIN_WINDOW_NAME)
+      add(sniper_status)
+      pack
+      set_default_close_operation(Swing::JFrame::EXIT_ON_CLOSE)
+      set_visible(true)
+    end
+
+    def show_status(text)
+      @sniper_status.set_text(text)
+    end
+
+    private
+    def create_label(initial_text)
+      label = Swing::JLabel.new(initial_text)
+      label.set_name(SNIPER_STATUS_NAME)
+      label.set_border(Swing::LineBorder.new(AWT::Color::BLACK))
+      label
+    end
+  end
 
   def self.connection(hostname, username, password)
     connection = Smack::XMPPConnection.new(hostname)
@@ -189,11 +199,11 @@ class ApplicationRunner
       end
     end
     @driver = AuctionSniperDriver.with_timeout(1000)
-    @driver.shows_sniper_status(AuctionSniper::STATUS_JOINING)
+    @driver.shows_sniper_status(AuctionSniper::MainWindow::STATUS_JOINING)
   end
 
   def shows_sniper_has_lost_auction
-    @driver.shows_sniper_status(AuctionSniper::STATUS_LOST)
+    @driver.shows_sniper_status(AuctionSniper::MainWindow::STATUS_LOST)
   end
 
   def stop
