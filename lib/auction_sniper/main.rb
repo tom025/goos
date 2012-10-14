1d43b7a6c6658ce808059c9c5953f7ce68cb0e8d
Tom Brand <tom025@me.com>
2012-10-14 15:58:18 +0000

    do not know why I cannot extract the auction class to own file

--- lib/auction_sniper/main.rb
+++ lib/auction_sniper/main.rb
@@ -3,30 +3,6 @@ require 'lib/swing'
 require 'lib/awt'
 require 'lib/auction_sniper/auction_message_translator'
 
-class Auction
-  JOIN_COMMAND_FORMAT = 'SOLVersion: 1.1; Command: Join;'
-  BID_COMMAND_FORMAT = 'SOLVersion: 1.1; Command: Bid; Amount: %d'
-
-  def initialize(chat)
-    @chat = chat
-  end
-
-  def bid(amount)
-    send_message(BID_COMMAND_FORMAT % amount)
-  end
-
-  def join
-    send_message(JOIN_COMMAND_FORMAT)
-  end
-
-  private
-  def send_message(message)
-    @chat.send_message(message)
-  rescue XMPPException => e
-    puts e.message
-  end
-end
-
 class AuctionSniper
   class Main
     AUCTION_RESOURCE = 'Auction'
@@ -79,6 +55,30 @@ class AuctionSniper
         connection.disconnect
       end
     end
-
   end
 end
+
+class Auction
+  JOIN_COMMAND_FORMAT = 'SOLVersion: 1.1; Command: Join;'
+  BID_COMMAND_FORMAT = 'SOLVersion: 1.1; Command: Bid; Amount: %d'
+
+  def initialize(chat)
+    @chat = chat
+  end
+
+  def bid(amount)
+    send_message(BID_COMMAND_FORMAT % amount)
+  end
+
+  def join
+    send_message(JOIN_COMMAND_FORMAT)
+  end
+
+  private
+  def send_message(message)
+    @chat.send_message(message)
+  rescue XMPPException => e
+    puts e.message
+  end
+end
+
