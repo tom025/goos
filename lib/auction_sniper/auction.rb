455bb17d08254874f3da7744df86a7052a16652c
Tom Brand <tom025@me.com>
2013-01-20 18:25:56 +0000

    Move auction to own file.

---
+++ lib/auction_sniper/auction.rb
@@ -0,0 +1,24 @@
+class Auction
+  JOIN_COMMAND_FORMAT = 'SOLVersion: 1.1; Command: Join;'
+  BID_COMMAND_FORMAT = 'SOLVersion: 1.1; Command: Bid; Amount: %d'
+
+  attr_reader :item_id
+  def initialize(chat)
+    @chat = chat
+  end
+
+  def bid(amount)
+    send_message(BID_COMMAND_FORMAT % amount)
+  end
+
+  def join
+    send_message(JOIN_COMMAND_FORMAT)
+  end
+
+  private
+  def send_message(message)
+    @chat.send_message(message)
+  rescue XMPPException => e
+    puts e.message
+  end
+end
--- lib/auction_sniper/main.rb
+++ lib/auction_sniper/main.rb
@@ -5,6 +5,7 @@ require 'lib/auction_sniper/auction_message_translator'
 require 'lib/auction_sniper/snipers_table_model'
 require 'lib/auction_sniper/swing_thread_sniper_listener'
 require 'lib/user_request'
+require 'lib/auction_sniper/auction'
 
 class AuctionSniper
   class Main
@@ -56,31 +57,6 @@ class AuctionSniper
         connection.disconnect
       end
     end
-  end
-end
-
-class Auction
-  JOIN_COMMAND_FORMAT = 'SOLVersion: 1.1; Command: Join;'
-  BID_COMMAND_FORMAT = 'SOLVersion: 1.1; Command: Bid; Amount: %d'
-
-  attr_reader :item_id
-  def initialize(chat)
-    @chat = chat
-  end
-
-  def bid(amount)
-    send_message(BID_COMMAND_FORMAT % amount)
-  end
-
-  def join
-    send_message(JOIN_COMMAND_FORMAT)
-  end
-
-  private
-  def send_message(message)
-    @chat.send_message(message)
-  rescue XMPPException => e
-    puts e.message
   end
 end
 
